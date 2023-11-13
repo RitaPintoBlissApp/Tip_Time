4976327476a0653c973aab440a4dde9e4bc0bb22
RitaPintoBissApp <rpinto@blissapplications.com>
2023-11-13 12:40:46 +0000

    Using remember function to save state

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -33,7 +33,10 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -74,7 +77,9 @@ fun TipTimeLayout() {
                 .padding(bottom = 16.dp, top = 40.dp)
                 .align(alignment = Alignment.Start)
         )
-        EditNumberField(modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth())
+        EditNumberField(modifier = Modifier
+            .padding(bottom = 32.dp)
+            .fillMaxWidth())
         Text(
             text = stringResource(R.string.tip_amount, "$0.00"),
             style = MaterialTheme.typography.displaySmall
@@ -95,10 +100,10 @@ private fun calculateTip(amount: Double, tipPercent: Double = 15.0): String {
 
 @Composable
 private fun EditNumberField(modifier: Modifier = Modifier){
-    var amountInput = mutableStateOf("0")
+    var amountInput by remember { mutableStateOf("0") }
     TextField(
-        value = amountInput.value,
-        onValueChange = {amountInput.value = it },
+        value = amountInput,
+        onValueChange = {amountInput = it },
         modifier = modifier)
 }
 
